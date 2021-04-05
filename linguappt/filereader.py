3a9a04d83951d4607b432d8b822e8fa80a6eaf7f
JingWeiLyu <murka@JingWeiLyus-MBP.powter>
2021-04-05 10:18:06 +0800

    merge readcsv into lib and remove unused package

--- linguappt/filereader.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-def readCSV(filename):
-  with open(filename) as csvfile:
-    readCSV = csv.reader(csvfile, skipinitialspace=True, delimiter='\t')
-    header = next(readCSV)
-    return [dict(zip(header, row)) for row in readCSV]
-
