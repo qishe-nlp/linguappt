ac0ebb71c6b4554e47db5f40b1ce44c2b08032ff
JingWeiLyu <murka@JingWeiLyus-MBP.powter>
2021-09-21 16:23:22 +0800

    use phrase ppt template without prep phrase

--- linguappt/__init__.py
+++ linguappt/__init__.py
@@ -54,7 +54,7 @@ This module demostrates the usage of package `linguappt`.
 
 """
 
-__version__ = '0.1.14'
+__version__ = '0.1.15'
 
 
 from .es.vocab_summary import SpanishVocabPPT
--- linguappt/en/phrase_summary.py
+++ linguappt/en/phrase_summary.py
@@ -15,49 +15,14 @@ class EnglishPhrasePPT(PhrasePPT):
   }
   lang = 'en'
 
-  content_keys = ["sentence", "noun_phrases", "prep_phrases", "verb_phrases", "verbs"]
+  content_keys = ["sentence", "noun_phrases", "verb_phrases", "verbs"]
 
   def __init__(self, sourcefile, title="", genre="classic"):
     super().__init__(sourcefile, title, genre)
 
-  def _create_three_kinds_phrase(self, line):
-    sentence_obj = json.loads(line["sentence"])
-    nps_obj = json.loads(line["noun_phrases"])[:3]
-    pps_obj = json.loads(line["prep_phrases"])[:3]
-    #vps_obj = json.loads(line["verb_phrases"])[:3]
-
-    layout = self._prs.slide_layouts.get_by_name("Three kinds of phrase")
-    slide = self._prs.slides.add_slide(layout)
-    holders = slide.shapes.placeholders
-  
-    st_holder, sm_holder = holders[10], holders[11]
-    st_holder.text_frame.text = sentence_obj["text"]
-    sm_holder.text_frame.text = sentence_obj["meaning"]
-
-    for index, np in enumerate(nps_obj):
-      base_index = 12 + index*2
-
-      t_holder, m_holder = holders[base_index], holders[base_index+1]
-      t_holder.text_frame.text = np["text"]
-      m_holder.text_frame.text = np["meaning"]
-
-    for index, pp in enumerate(pps_obj):
-      base_index = 18 + index*2
-
-      t_holder, m_holder = holders[base_index], holders[base_index+1]
-      t_holder.text_frame.text = pp["text"]
-      m_holder.text_frame.text = pp["meaning"]
-
-    #for index, vp in enumerate(vps_obj):
-    #  t_holder, m_holder = holders[24+index], holders[25+index]
-    #  t_holder.text_frame.text = vp["text"]
-    #  m_holder.text_frame.text = vp["meaning"]
-
   def _create_phrase_and_verb(self, line):
     sentence_obj = json.loads(line["sentence"])
-    nps_obj = json.loads(line["noun_phrases"])[:2]
-    pps_obj = json.loads(line["prep_phrases"])[:2]
-    ps_obj = nps_obj + pps_obj
+    ps_obj = json.loads(line["noun_phrases"])[:4]
     vs_obj = json.loads(line["verbs"])[:4]
 
     layout = self._prs.slide_layouts.get_by_name("Phrase and verb")
@@ -84,7 +49,7 @@ class EnglishPhrasePPT(PhrasePPT):
       form_holder.text_frame.text = v["form"].split(",")[1]
  
   def _create_phrase(self):
-    """Create phrase by sentence, which are noun phrases, prep phrases, verb phrases
+    """Create phrase by sentence, which are noun phrases, verb phrases
     """
     for line in self.content:
       self._create_phrase_and_verb(line)
