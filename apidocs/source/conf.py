065e74df9fbd335d1e359aaca114c8bf535f14df
JingWeiLyu <murka@JingWeiLyus-MBP.powter>
2021-10-22 15:45:41 +0800

    update templates

--- apidocs/source/conf.py
+++ apidocs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Phoenix Grey'
 author = 'Phoenix Grey'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.16'
+release = '0.1.17'
 
 
 # -- General configuration ---------------------------------------------------
--- linguappt/__init__.py
+++ linguappt/__init__.py
@@ -54,7 +54,7 @@ This module demostrates the usage of package `linguappt`.
 
 """
 
-__version__ = '0.1.16'
+__version__ = '0.1.17'
 
 
 from .es.vocab_summary import SpanishVocabPPT
